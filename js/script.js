203d082ddd3c5b0f8673b1b592c4f4d0ac968326
PedroRocs <pedrorochacardoso1908@gmail.com>
2022-05-18 22:44:48 -0300

    criando evento onde muda a imagem da main para outra com resolução diferente de acordo com o tamanho da tela

--- js/script.js
+++ js/script.js
@@ -7,16 +7,19 @@ function abrirMenuResponsivo(){
 }
 
 function abrirDropdown(parametro){
-    
     let menuDropdown = document.querySelectorAll(".dropdown");
     menuDropdown[parametro].classList.toggle("active");
-    
- 
-
-    
-
-    
-
 }
 
+window.addEventListener("resize", ()=>{
 
+let img = document.querySelector("#content-img img");
+
+if(window.screen.width<989){
+    img.src="images/image-hero-mobile.png";
+}
+if(window.screen.width>989){
+    img.src="images/image-hero-desktop.png";
+    console.log("1")
+}
+})
